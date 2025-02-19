091b2c68ce8822f8bd36af1fe4f08a2e74cf5612
yogis <yogisyo16@gmail.com>
2025-02-19 09:43:55 +0700

    navbar update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Macondo, Raleway } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+import classnames from "classnames";
 
 const macondo = Macondo({
   variable: "--font-macondo",
@@ -46,20 +46,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="icon" content="/heart.ico" />
-        <title>My Page Title</title>
-      </Head>
-      <body className={raleway.className}>
+      <body className={(classnames(raleway.className), "bg-indigo-950/70")}>
         <nav className="flex-nowrap fixed top-0 z-10 w-full">
-          <div className="mx-aute max-w-7x1 px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-20 items-center justify-between">
-              <div>
-                <a href="#" className={macondo.className}>
-                  Yogiswara Adinugraha Pramudita
-                </a>
-              </div>
-              <ul className="flex flex-collist-none align-middle">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a
+              href="#"
+              className={
+                (classnames(macondo.className),
+                "bg-gradient-to-r from-gray-100 via-zinc-300 to-stone-400 text-transparent bg-clip-text text-3xl")
+              }
+            >
+              PortGis
+            </a>
+            <button
+              data-collapse-toggle="navbar-portgis"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-portgis"
+              aria-expanded="false"
+            >
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-portgis"
+            >
+              <ul className="flex align-middle md:flex-row md:space-x-8 rtl:space-x-reverse">
                 <li className="mr-4">
                   <a href="#">About</a>
                 </li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 md:items-center sm:items-start">
         <div className="flex flex-col gap-2">
           <h1 className="text-4xl">Yogiswara Adinugraha Pramudita</h1>
-          <h2 className="text-2xl text-gray-500">Full-Stack Developer</h2>
+          <h2 className="text-2xl text-gray-400">Full-Stack Developer</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
             dignissimos incidunt amet itaque quos alias!
@@ -38,20 +38,20 @@ export default function Home() {
         <div className="flex flex-col mt-20 gap-4">
           <h1 className="text-5xl">Projects</h1>
           <div className="flex flex-row gap-14">
-            <div className="max-w-sm p-6 bg-white border bnorder-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 gap-2">
-              <h2 className="text-3xl">PT Lendis Cipta Media Jaya</h2>
+            <div className="max-w-sm p-6 bg-white border bnorder-gray-200 rounded-lg shadow-sm dark:bg-slate-500 dark:border-gray-700 gap-2">
+              <h2 className="text-3xl">PT Lendis Cipta Media</h2>
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
                 incidunt?
               </p>
               <a
                 href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-900 focus:ring-4 foucus:outline-none focus:ring-indigo-700 dark:hover:bg-indigo-600 dark:focus:ring-indigo-800"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-800 rounded-lg hover:bg-cyan-600 focus:ring-4 foucus:outline-none focus:ring-cyan-800 dark:hover:bg-cyan-600 dark:focus:ring-cyan-900"
               >
                 Go To Projects
               </a>
             </div>
-            <div className="max-w-sm p-6 bg-white border bnorder-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 gap-2">
+            <div className="max-w-sm p-6 bg-white border bnorder-gray-200 rounded-lg shadow-sm dark:bg-slate-500 dark:border-gray-700 gap-2">
               <h2 className="text-3xl">Apple Academy</h2>
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
@@ -59,7 +59,7 @@ export default function Home() {
               </p>
               <a
                 href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-900 focus:ring-4 foucus:outline-none focus:ring-indigo-700 dark:hover:bg-indigo-600 dark:focus:ring-indigo-800"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-800 rounded-lg hover:bg-cyan-600 focus:ring-4 foucus:outline-none focus:ring-cyan-800 dark:hover:bg-cyan-600 dark:focus:ring-cyan-900"
               >
                 Go To Projects
               </a>
