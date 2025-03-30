598e0c8e945b3b07705375a28230793038485b51
yogis <yogisyo16@gmail.com>
2025-03-30 21:44:07 +0700

    eficiency code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,8 +42,8 @@ export default function RootLayout({
         className={(classnames(raleway.className), "bg-indigo-900/70 glass")}
       >
         <div className="flex flex-col min-w-screen min-h-screen">
-          <nav className="navbar-center mb-24">
-            <div className="max-w-screen-2xl flex items-start justify-between mx-auto p-4">
+          <nav className="navbar mb-20 md:items-start">
+            <div className="flex-1 mx-auto p-4">
               <Link
                 href="/"
                 className={
@@ -57,45 +57,33 @@ export default function RootLayout({
                   className="w-16 md:w-32 bg-white rounded-full"
                 />
               </Link>
-              <button
-                data-collapse-toggle="navbar-portgis"
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-portgis"
-                aria-expanded="false"
-              >
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 17 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 1h15M1 7h15M1 13h15"
-                  />
-                </svg>
-              </button>
-              <div
-                className="hidden w-full md:block md:w-auto"
-                id="navbar-portgis"
-              >
-                <ul className="menu menu-horizontal md:flex-row md:space-x-8 rtl:space-x-reverse">
-                  <li className="mr-2">
+            </div>
+            <div className="flex-none md:flex md:flex-col">
+              <details className="md:hidden">
+                <summary className="btn m-2">Menu</summary>
+                <ul className="bg-base-100 rounded-2xl p-2 items-center">
+                  <li className="mb-2">
                     <Link href="/about">About</Link>
                   </li>
-                  <li className="mr-4">
+                  <li className="mb-2">
                     <Link href="/projects">Projects</Link>
                   </li>
-                  <li className="mr-4">
+                  <li className="">
                     <Link href="/contact">Contact</Link>
                   </li>
                 </ul>
-              </div>
+              </details>
+              <ul className="hidden menu menu-dropdown md:flex md:flex-row md:space-x-8 md:m-6 ">
+                <li className="md:mr-2">
+                  <Link href="/about">About</Link>
+                </li>
+                <li className="md:mr-4">
+                  <Link href="/projects">Projects</Link>
+                </li>
+                <li className="md:mr-4">
+                  <Link href="/contact">Contact</Link>
+                </li>
+              </ul>
             </div>
           </nav>
           <Transition>{children}</Transition>
